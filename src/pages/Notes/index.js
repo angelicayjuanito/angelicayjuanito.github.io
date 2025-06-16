eeadec983b80c34b755a19a0e517cf879be036ea
Juan Martinez <167187217+juan-martinez-onum@users.noreply.github.com>
2025-06-16 15:50:30 +0200

    feat: add FullScreenPage.js component. Add each component for secions. Apply styles

---
+++ src/pages/Notes/index.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import styles from "./index.module.css";
+
+export default function Notes() {
+    return (
+        <div className={styles.container}>Notes</div>
+    )
+}
---
+++ src/pages/Welcome/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import styles from './index.module.css';
+
+export const Welcome = () => {
+    return (
+        <div className={styles.container}>
+            <h3 className={styles.subtitle}>Una historia escrita y dirigida por</h3>
+            <h1 className={styles.title}>ANGÉLICA Y JUANITO</h1>
+        </div>
+    )
+}
