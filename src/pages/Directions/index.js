ebd78363d47a8cd95d56f71dabebedc03d3c1d08
Juan Martinez <167187217+juan-martinez-onum@users.noreply.github.com>
2025-06-20 10:19:58 +0200

    Updates

--- src/pages/Directions/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import styles from "./index.module.css";
-
-export const Directions = () => {
-    return (
-        <div className={styles.container}>Directions</div>
-    )
-}
--- src/pages/Recommendations/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import styles from './index.module.css';
-
-export const Recommendations = () => {
-    return (
-        <div className={styles.container}>Recommendations</div>
-    )
-}
